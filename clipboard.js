cffe86c2a0fdb78e71b48bb6fd3e1168fa5fd7de
문한주(163229) <luvet@shinsegae.com>
2020-02-14 22:38:37 +0900

    [2주차] 코드리뷰 (이벤트 위임, 리스너함수, 리스너제거, 상태토글, 필터/소트 리팩토링, 모수리스트 통합)

--- clipboard.js
+++ clipboard.js
@@ -152,32 +152,55 @@ const grid = await (async ($parent, url) => {
         $el = $parent.lastElementChild;
 
         // dom 엘리먼트 변수명 선언
+        /* TODO 동일한 성격의 엘리먼트에 유사한 로직이 리스너로 붙을 경우,
+        로직을 공통화하고, 부모 엘리먼트에 이벤트를 위임 해주는 것이 성능/구조 측면에서 모두 좋습니다 */
         $latestSortBtn = $parent.querySelector('article').firstElementChild.children[0];
         $popularSortBtn = $parent.querySelector('article').firstElementChild.children[1];
         $searchInp = $parent.querySelector('article').firstElementChild.children[2].querySelector('input');
 
+        /* BUG 이벤트리스너는 화살표함수가 아닌, 일반함수로 사용하셔야 합니다
+        이 글 내용 참조 해보세요 https://mengkkoit.tistory.com/23 */
+        /* FIXME 컴포넌트 프로그래밍에서는, 모든 '추가' 동작은 '제거' 가능해야 합니다
+        리스너를 별도의 함수로 분리하고 (또는 리스너 객체를 만들어서, 그 하위 메소드로 몰아도 좋구요)
+        해당 리스너를 add/reomve 할 수 있도록 리팩토링 해주세요 */
+        /* FIXME 상태값을 DOM에 넣었다 뺐다 하는 건 바람직하지 않습니다 (넣더라도 data속성으로..)
+        가능하면 상태 자체가 없는 게 좋은데, 일단 클로저 기반으로 리팩토링까지만 했습니다 */
         // sort 클릭이벤트 매핑
         // 클래스명을 toggle로 주고, 클래스명 여부에 따라 함수에 적합한 파라미터 전달
-        $latestSortBtn.addEventListener('click', ()=>{
-            $el.classList.toggle('latest-sort');
-            if($el.classList.contains('latest-sort')){
-                sort('latest');
-            }else{
-                sort('oldest');
-            }
-        });
+        $latestSortBtn.addEventListener('click', (() => {
+            let isLatest = true;
 
-        $popularSortBtn.addEventListener('click', ()=>{
-            $el.classList.toggle('popular-sort');
-            if($el.classList.contains('popular-sort')){
-                sort('popular');
-            }else{
-                sort('unpopular');
+            return (e) => {
+                if(isLatest) {
+                    sort('oldest');
+                } else {
+                    sort('latest');
+                }
+                isLatest = !isLatest;
             }
-        });
+        })());
 
+        // BUG 이벤트리스너 교체
+        // FIXME 리스너 제거 가능하도록 분리
+        $popularSortBtn.addEventListener('click', (() => {
+            let isPopular = false;
+
+            return (e) => {
+                if(isPopular) {
+                    sort('unpopular');
+                } else {
+                    sort('popular');
+                }
+                isPopular = !isPopular;
+            }
+        })());
+
+        // BUG 이벤트리스너 교체
+        // FIXME 리스너 제거 가능하도록 분리
         // keyup 이벤트 발생시 filter 함수 실행. 인풋의 값을 파라미터로 전달
-        $searchInp.addEventListener('keyup', () => filter($searchInp.value) );
+        $searchInp.addEventListener('keyup', (e) => {
+            filter(e.currentTarget.value);
+        });
     }
 
     const divide = (list, size) => {
@@ -201,134 +224,54 @@ const grid = await (async ($parent, url) => {
     let listList = divide(timelineList, ITEM_PER_ROW);
 
     // filterList: filter에서 걸러진 값을 담을 변수
-    let filterList = [];
+    /* TODO 고려해야 할 리스트는, 원본리스트/템프리스트 두 개면 충분할 것 같습니다 */
+    let resultList = timelineList;
 
+    /* TODO 필터결과 판단하는 기존로직이 복잡해서, 간단하게 로직 리팩토링 했습니다
+    특정한 데이터를 캐싱(여기서는 filterList를 캐싱)하는 로직은
+    가공 전의 데이터를 기준으로 해주세요(divide함수 들어간 후에서, 전으로 변경)*/
     const filter = (keyword) => {
-        // TODO 검색창 input에 key이벤트 발생시 검색로직 수행
         $el.lastElementChild.firstElementChild.innerHTML = '';
         
-        
-        let filterResult = divide(timelineList.filter(i =>{
-            if(i.text.includes(keyword)){
-                return i;
-            }
-        }), ITEM_PER_ROW);
-        
-        if(filterResult == undefined) return;
-        filterList = [];
-        filterResult.forEach(list => {
-            filterList.push(list);
-        });
+        resultList = timelineList.filter(i => (i.text + i.name).includes(keyword));
+        renderRows(divide(resultList, ITEM_PER_ROW));
 
-        filterRefreshTrigger();
-        return {filterList};
+        return { resultList };
     }
-    
-    function filterRefreshTrigger(){
-        listList = filterList;
-        setRow(listList);
-    }
-    
-    let sortTargetList=[];
-    let sortList = [];
+
+    /* TODO 정렬기준 판단하는 기존로직이 복잡해서, 적절한 패턴 활용해서 리팩토링 했습니다
+    기존에 짜신 게 잘못된 것이 아니니 절대 실망하시면 안 돼요! 기존로직도 잘 짜셨습니다
+    더 견고하고 읽기 쉽고 확장하기 쉽게 바꿀 수도 있다는 점만 참고 해보세요~ */
+    const comparator = (() => {
+        const computePopularity = data => Number.parseInt(data.clipCount) + Number.parseInt(data.commentCount) * 2;
+        const parseTimeValue = data => new Date(data.timestamp).getTime();
+
+        return {
+            popular: (a, b) => computePopularity(b) - computePopularity(a),
+            unpopular: (a, b) => computePopularity(a) - computePopularity(b),
+            latest: (a, b) => parseTimeValue(b) - parseTimeValue(a),
+            oldest: (a, b) => parseTimeValue(a) - parseTimeValue(b),
+        }
+    })();
     const sort = (sortKind) => {
-        // filter 검색한 결과에 sort를 적용할 경우
-        if(!!$searchInp.value){
-            let copy = [...filterList];
-    
-            sortTargetList=[];
-            copy.forEach(list => {
-                list.forEach(item => {
-                    sortTargetList.push(item);
-                });
-            });
-        }else{
-            sortTargetList = timelineList;
-        }
-
-        // TODO 최신순/인기순 클릭시 해당 정렬로직 수행
-        let popularityOrderSort,
-            popularityDisorderSort,
-            latestSort,
-            ordestSort;
-
-        popularityOrderSort = (a, b) => {
-            num1 = Number.parseInt(a.clipCount) + Number.parseInt(a.commentCount);
-            num2 = Number.parseInt(b.clipCount) + Number.parseInt(b.commentCount);
-            
-            if(num1 > num2) return -1;
-            if(num1 < num2) return 1;
-            return 0;
-        }
-            
-        popularityDisorderSort = (a, b) => {
-            num1 = Number.parseInt(a.clipCount) + Number.parseInt(a.commentCount);
-            num2 = Number.parseInt(b.clipCount) + Number.parseInt(b.commentCount);
-
-            if(num1 > num2) return 1;
-            if(num1 < num2) return -1;
-            return 0;
-        }
-        
-        latestSort = (a, b) => {
-            var time1 = new Date(a.timestamp).getTime();
-            var time2 = new Date(b.timestamp).getTime();
-            if(time1 > time2){
-                return 1;
-            }
-            if(time1 < time2){
-                return -1;
-            }
-            return 0;
-        }
-
-        ordestSort = (a, b) => {
-            var time1 = new Date(a.timestamp).getTime();
-            var time2 = new Date(b.timestamp).getTime();
-            if(time1 > time2){
-                return -1;
-            }
-            if(time1 < time2){
-                return 1;
-            }
-            return 0;
-        }
-        
-        // 파라미터에 따라 필요한 sort 함수 mapping
-        let sortCase;
-        switch(sortKind){
-            case 'popular': 
-                sortCase = popularityOrderSort;
-                break;
-            case 'unpopular': 
-                sortCase = popularityDisorderSort;
-                break;
-            case 'latest':
-                sortCase = latestSort;
-                break;
-            case 'oldest':
-                sortCase = ordestSort;
-                break;
-        }
-
+        /* FIXME 소트로직에서 $searchInp.value에 직접 접근하는 것은 바람직하지 않습니다
+        정렬여부에 따라 바라보고 있는 모수 리스트 자체를 바꾸도록 리팩토링 했습니다 */
         $el.lastElementChild.firstElementChild.innerHTML = '';
 
-        // 컨텐츠 들어갈 곳에 fetch data를 데이터 포맷팅해서 형식대로 넣기
-        sortList = [];
-        divide(sortTargetList.sort(sortCase), ITEM_PER_ROW)
-           .forEach(list => {
-            sortList.push(list);
-           });
-           
-        // 새로고침 트리거 실행
-        sortRefreshTrigger();
-        return {sortList};
+        timelineList.sort(comparator[sortKind]);
+        resultList.sort(comparator[sortKind]);
+        renderRows(divide(resultList, ITEM_PER_ROW));
+
+        return { resultList };
     }
     
-    function sortRefreshTrigger(){
-        listList = sortList;
-        setRow(listList);
-    }
+    /* FIXME 함수가 함수 외부의 뮤터블한 값에 접근하는 것은 지양 해주세요 (페이지 같은 경우를 제외하고)
+    아마 개발하실 때도 오래 걸리셨을 거고, 추후 유지보수 시에도 오래 걸립니다
+    영향범위를 한정하여 견고하게 동작할 수 있도록, 되도록 주요한 데이터는 파라미터로 직접 받아주세요
+    (refresh 함수 자체를 없애서, 최종코드 기준으로 주석처리 했습니다) */
+    // function refresh(resultList){
+    //     renderRows(resultList);
+    // }
 
     const render = () => {
         $parent.insertAdjacentHTML('beforeend', `
@@ -370,8 +313,12 @@ const grid = await (async ($parent, url) => {
     return { $el, listList }
 })(timelineContent.$el.firstElementChild, timeline.url);
 
+// TODO setRow는 비동기 함수가 아닌 것 같아서, async 뺐습니다 (수정완료)
+/* FIXME setRow의 디폴트로 grid.listList가 오기 보다는
+호출시에 넘겨주는 게 재사용 측면에서 더 바람직할 것 같습니다
+디폴트는 빈배열로 교체 했습니다 (수정완료) */
 // list 그려주는 setRow 함수
-async function setRow(listList = grid.listList){
+function renderRows(listList = []) {
     listList.forEach(list => {
         const gridItem = (($parent, list) => {
             let $el;
@@ -410,7 +357,7 @@ async function setRow(listList = grid.listList){
         })(grid.$el.lastElementChild.firstElementChild, list);
     });
 };
-setRow();
+renderRows(grid.listList);
     
 
 })();
